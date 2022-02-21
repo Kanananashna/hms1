907ea9db0b647883fee61fd5040d694d41aaad78
mweshnana <99644993+mweshnana@users.noreply.github.com>
2022-02-21 16:29:52 +0300

    first commit

---
+++ src/main/java/Model/Client.java
@@ -0,0 +1,17 @@
+package Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "client")
+public class Client {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+    private String email;
+
+}
---
+++ src/main/java/com/example/hms/ClientsController.java
@@ -0,0 +1,53 @@
+package com.example.hms;
+
+import Model.Client;
+import Model.ClientRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.print.attribute.URISyntax;
+import javax.xml.ws.Response;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/clients")
+
+public class ClientsController {
+    private final ClientRepository clientRepository;
+
+    public ClientsController(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+    @GetMapping
+    public List<Client> getClients(){
+        return ClientRepository.findAll();
+    }
+    @GetMapping("/{id}")
+    public Client getClient(@PathVariable Long id){
+        return ClientRepository.findById(id).orElseThrow(RuntimeException::new);
+
+    }
+    @PostMapping
+    public ResponseEntity createClient(@RequestBody Client client) throws URISyntaxException{
+        Client savedClient= clientRepository.save(client);
+        return ResponseEntity.created(new URI("/clients" +savedClient.getId())).body(savedClient);
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity updateClient(@PathVariable Long id @RequestBody Client client){
+        Client currentClient= clientRepository.findById(id).orElseThrow(RuntimeException::new);
+        currentClient.setName(client.getName());
+        currentClient.setEmail(client.getEmail());
+        currentClient=clientRepository.save(client);
+        return ResponseEntity.ok(currentClient);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteClient(@PathVariable Long id){
+        clientRepository.deleteById(id);
+        return ResponseEntity.ok().build();
+    }
+
+
+
+}
